cad1b4b9e3f07db4f4afea493ec1031e07a8c0c6
uduan17 <josehut4020@gmail.com>
2022-11-01 15:12:40 -0500

    Firebase-Config

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react'
+import { collection, getDocs } from "firebase/firestore";
+import db from './firebase/FirebaseConfig'
 
-function App() {
+const App = () => {
+
+  useEffect(() => {
+
+    const obtenerdatos = async() => {
+      const datos = await getDocs(collection(db, 'usuarios'));
+      datos.forEach(documento => {
+        console.log(documento.data());
+      });
+    }
+    obtenerdatos();
+  }, []);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div>Holas desde fire base</div>
+  )
 }
 
-export default App;
+export default App
